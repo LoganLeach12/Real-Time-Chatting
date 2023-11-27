d0db6d4d58a52469173c544e3f444f706c4710a5
LoganLeach <122493729+LoganLeach12@users.noreply.github.com>
2023-11-26 20:13:38 -0500

    Updated the server.js file
    updated the server.js file so it works with the localhost that render supports and connects to the socket.io server.

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const userNames = {};
+
+// Serve static files
+app.use(express.static(__dirname));
+
+// Socket.io events
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  // Broadcast to all clients when a user joins
+  socket.on("user joined", (username) => {
+    userNames[socket.id] = username;
+    socket.broadcast.emit("user joined", username);
+  });
+
+  socket.on("chat message", (msgData) => {
+    io.emit("chat message", msgData);
+  });
+
+  socket.on("disconnect", () => {
+    const username = userNames[socket.id] || "A user";
+    console.log(`${username} disconnected`);
+    io.emit("user left", username);
+    delete userNames[socket.id];
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log(`Server is running on https://real-time-chatting.onrender.com:${PORT}`);
+});
